2782efd31530e9cd8c643c47a80c4bbbef554cb3
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-05-03 20:38:31 +0530

    Implement service worker with caching and notifications

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
 
+const CACHE_NAME = 'talk-task-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) return response;
+                return fetch(event.request);
+            })
+    );
+});
+
+// Listen for messages from the React app to trigger background OS notifications
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+        self.registration.showNotification(event.data.title, {
+            body: event.data.body,
+            icon: 'https://cdn-icons-png.flaticon.com/512/825/825590.png',
+            vibrate: [200, 100, 200]
+        });
+    }
+});

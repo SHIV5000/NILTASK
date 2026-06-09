2aceddb18c623e6de4a95105b453ce491a83c7b2
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-06-09 09:16:08 +0530

    Add subscription setter functions to state.js

--- js/state.js
+++ js/state.js
@@ -1,5 +1,8 @@
+// Global state variables
 export let currentUser = null;
 export let currentRoom = 'general';
+
+// Subscriptions
 export let messageSubscription = null;
 export let taskSubscription = null;
 export let assigneeSubscription = null;
@@ -7,6 +10,12 @@ export let trailSubscription = null;
 export let notificationSubscription = null;
 export let bookmarkSubscription = null;
 
+// Setter functions
 export function setCurrentUser(user) { currentUser = user; }
 export function setCurrentRoom(room) { currentRoom = room; }
-// Subscription setters similarly
+export function setMessageSubscription(sub) { messageSubscription = sub; }
+export function setTaskSubscription(sub) { taskSubscription = sub; }
+export function setAssigneeSubscription(sub) { assigneeSubscription = sub; }
+export function setTrailSubscription(sub) { trailSubscription = sub; }
+export function setNotificationSubscription(sub) { notificationSubscription = sub; }
+export function setBookmarkSubscription(sub) { bookmarkSubscription = sub; }

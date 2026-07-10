d5784ff06b3978a75c4a8d59966bc8cd6562bdc7
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-07-10 07:54:25 +0530

    Update mobile version from v139 to v140

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,7 @@
 import { sb } from './shared.js';
 
 const MOB = 768;
-const _MOB_VER = 'v139';
+const _MOB_VER = 'v140';
 
 // Console log buffer — tap version badge to copy all logs
 const _logBuf = [];
